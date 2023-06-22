f9ea8c9e91fa81e57240fcda5cafbd5ad97448b5
B-litljon <brandonC.littlejohn@gmail.com>
2023-06-22 16:35:36 -0700

    db connected to app, mongoose schema has been made, server updated

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,10 +7,15 @@ const errorHandler = require('./middleware/errorHandeler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConnection')
+const mongoose = require('mongoose')
+const { logEvents } = require('./middleware/logger')
 
 const PORT = 3000 || process.env.PORT
 
 console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(logger)
 
@@ -24,6 +29,8 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
 
+app.use('/users', require('./routes/userRoutes'))
+
 app.all('*', (req, res) => {
    res.status(404)
    if (req.accepts('html')) {
@@ -34,8 +41,15 @@ app.all('*', (req, res) => {
       res.type('txt').send('404 not found')
    }
 })
+
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-   console.log(`Server is servin on PORT: ${PORT} !`)
+mongoose.connection.once('open', () => {
+   console.log('Connection to mongodb was succesful!')
+   app.listen(PORT, () => {console.log(`Server is servin on PORT:"${PORT}" !`)})
+})
+
+mongoose.connection.on('error', err => {
+   console.log(err)
+   logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
